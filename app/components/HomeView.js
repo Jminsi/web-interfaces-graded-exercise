870fa732f62a44a1a3e97b0eea1a13b52eb83b8d
Minsi Jamsa <K8jami00@students.oamk.fi>
2021-02-24 02:02:01 +0200

    Added two views: home and postings for the app

---
+++ app/components/HomeView.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const HomeView = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text style={{ fontSize: 50, fontWeight: '700' }}>Home View</Text>
+    </View>
+  )
+}
+
+export default HomeView
