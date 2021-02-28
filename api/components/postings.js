58c9f3c1f4f57de3a3d5296c3b43e50b57ad8721
Minsi Jamsa <K8jami00@students.oamk.fi>
2021-02-28 13:46:30 +0200

    Rework with express api, implemented postings handling

---
+++ api/components/postings.js
@@ -0,0 +1,243 @@
+const express = require('express');
+const has = require('has-value');
+const router = express.Router();
+
+function validateJSONHeaders(req, res, next)
+{
+    if(req.get('Content-Type') === 'application/json')
+    {
+        next();
+    }
+    else
+    {
+        const err = new Error('Bad Request - Missing Headers');
+        err.status = 400;
+        next(err);
+    }
+}
+
+//  Some fixed example data of dogs
+let dogData = {
+    dogs: [{
+        id: 1,
+        name: "Max",
+        image: "https://media.giphy.com/media/4Zo41lhzKt6iZ8xff9/giphy.gif"
+    },
+    {
+        id: 2,
+        name: "Bella",
+        image: "https://media.giphy.com/media/3ndAvMC5LFPNMCzq7m/giphy.gif"
+    },
+    {
+        id: 3,
+        name: "Tucker",
+        image: "https://media.giphy.com/media/bbshzgyFQDqPHXBo4c/giphy.gif"
+    }]
+}
+
+
+let postingsData = {
+    postings: [
+    {
+      id: 1,
+      title: "Old Toyota Corolla1",
+      description: "Selling my Toyota Corolla Le, with only 90k original miles and Clean Title, never been any accidents, garage keeping.",
+      category: "cars",
+      location: "Oulu",
+      images: "no",
+      price: "123",
+      date: "27.02.2021 15:42",
+      delivery: "pickup",
+      seller: "Jack",
+      contact: "abc@zfsd.com / 050 123 123 123"
+    },
+    {
+      id: 2,
+      title: "Old Toyota Corolla2",
+      description: "Selling my Toyota Corolla Le, with only 90k original miles and Clean Title, never been any accidents, garage keeping.",
+      category: "cars",
+      location: "Oulu",
+      images: "no",
+      price: "123",
+      date: "27.02.2021 15:42",
+      delivery: "pickup",
+      seller: "Jack",
+      contact: "abc@zfsd.com / 050 123 123 123"
+    },
+    {
+      id: 3,
+      title: "Old Toyota Corolla3",
+      description: "Long description here",
+      category: "cars",
+      location: "Oulu",
+      images: "no",
+      price: "123",
+      date: "27.02.2021 15:42",
+      delivery: "pickup",
+      seller: "Jack",
+      contact: "abc@zfsd.com / 050 123 123 123"
+    },
+    {
+      id: 4,
+      title: "Old Toyota Corolla4",
+      description: "Selling my Toyota Corolla Le, with only 90k original miles and Clean Title, never been any accidents, garage keeping.",
+      category: "cars",
+      location: "Oulu",
+      images: "no",
+      price: "123",
+      date: "27.02.2021 15:42",
+      delivery: "pickup",
+      seller: "Jack",
+      contact: "abc@zfsd.com / 050 123 123 123"
+    },
+    {
+      id: 5,
+      title: "Old Toyota Corolla5",
+      description: "Selling my Toyota Corolla Le, with only 90k original miles and Clean Title, never been any accidents, garage keeping.",
+      category: "cars",
+      location: "Oulu",
+      images: "no",
+      price: "123",
+      date: "27.02.2021 15:42",
+      delivery: "pickup",
+      seller: "Jack",
+      contact: "abc@zfsd.com / 050 123 123 123"
+    },
+    {
+      id: 6,
+      title: "Old Toyota Corolla6",
+      description: "Selling my Toyota Corolla Le, with only 90k original miles and Clean Title, never been any accidents, garage keeping.",
+      category: "cars",
+      location: "Oulu",
+      images: "no",
+      price: "123",
+      date: "27.02.2021 15:42",
+      delivery: "pickup",
+      seller: "Jack",
+      contact: "abc@zfsd.com / 050 123 123 123"
+ },
+ {
+    id: 7,
+    title: "Shoes",
+    description: "My old shoes",
+    category: "clothes",
+    location: "Oulu",
+    images: "no",
+    price: "123",
+    date: "27.02.2021 15:42",
+    delivery: "pickup",
+    seller: "Jack",
+    contact: "abc@zfsd.com / 050 123 123 123"
+}
+
+]
+}
+
+
+
+//  Add posting
+router.get('/add/', (req, res) => { 
+    //console.log('\n  ADDDD zap=' + zap);
+    //zap = zap + 1
+    //postingsData = postingsData.pop();
+    //postings = postings.pop();
+    delete global.postings[0]; 
+    res.sendStatus(200);
+})
+
+
+//  Return all postings
+router.get('/', (req, res) => { 
+    //res.json(postingsData.postings)});
+    //console.log('\n  get zap=' + zap);
+    res.json(global.postings)
+})
+
+//  Return all postings for searched category
+router.get('/category/:category', (req, res) => {
+    console.log('\n  category:' + req.params.category);
+    const resultPostings = postingsData.postings.filter(p => {
+            return p.category == req.params.category;
+          });
+    if(resultPostings === undefined)
+    {
+        res.sendStatus(404)
+    }
+    else
+    {
+        res.json(resultPostings);
+    }
+})
+
+
+
+//  Return information of a single dog
+router.get('/:dogId', (req, res) => {
+    const resultDog = dogData.dogs.find(d => {
+        if (d.id == req.params.dogId) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    });
+    if(resultDog === undefined)
+    {
+        res.sendStatus(404)
+    }
+    else
+    {
+        res.json(resultDog);
+    }
+})
+
+/* Middleware to validate new dog creation */
+function validateNewDog(req, res, next)
+{
+    // prepare error object
+    const err = new Error();
+    err.name = "Bad Request";
+    err.status = 400;
+    if(has(req.body, 'name') == false)
+    {
+        err.message = "Missing or empty name";
+        next(err);
+    }
+    if(has(req.body, 'image') == false)
+    {
+        err.message = "Missing or empty image";
+        next(err);
+    }
+    next(); // no validation errors, so pass to the next
+}
+
+/* Create a new dog
+    Expects the following data format
+    {
+        name: string,
+        image: string - whole url to image
+    }
+*/
+router.post('/',
+    [
+      validateJSONHeaders,
+      validateNewDog
+    ],
+    (req, res) => {
+        const newDog = {
+            id: dogData.dogs.length + 1,
+            name: req.body.name,
+            image: req.body.image
+        }
+        dogData.dogs.push(newDog);
+
+        res.status(201);
+        res.json(newDog);
+});
+
+router.delete('/:id', (req, res) => {
+    dogData.dogs = dogData.dogs.filter(dog => dog.id != req.params.id);
+    res.sendStatus(200);
+})
+
+module.exports = router;
--- api/index.js
+++ api/index.js
@@ -2,12 +2,132 @@ const express = require('express');
 const app = express();
 const port = 4000;
 
-const dogComponent = require('./components/dogs');
-const imageUpload = require('./components/imageUpload');
+const postingsComponent = require('./components/postings');
+//const imageUpload = require('./components/imageUpload');
 const bodyParser = require('body-parser');
-const apiKeyDemo = require('./components/apiKeyDemo');
+//const apiKeyDemo = require('./components/apiKeyDemo');
 const cors = require('cors');
-const jsonSchemaValidationExample = require('./components/jsonSchemaValidationExample');
+//const jsonSchemaValidationExample = require('./components/jsonSchemaValidationExample');
+
+
+
+//   Simulates database for users
+global.users = [
+    {
+      id: 1,
+      email: "demo",
+      password: "demo",
+      username: "Demo User",
+      contact: "GSM: 11 111 111",
+    },
+    {
+      id: 2,
+      email: "demo2",
+      password: "demo2",
+      username: "Demo User 2",
+      contact: "GSM: 22 222 222",
+    }
+];
+
+
+//   Simulates database for postings
+global.postings = [
+    {
+      id: 1,
+      title: "Old Toyota Corolla11",
+      description: "Selling my Toyota Corolla Le, with only 90k original miles and Clean Title, never been any accidents, garage keeping.",
+      category: "cars",
+      location: "Oulu",
+      images: "no",
+      price: "123",
+      date: "27.02.2021 15:42",
+      delivery: "pickup",
+      seller: "Jack",
+      contact: "abc@zfsd.com / 050 123 123 123"
+    },
+    {
+      id: 2,
+      title: "Old Toyota Corolla2",
+      description: "Selling my Toyota Corolla Le, with only 90k original miles and Clean Title, never been any accidents, garage keeping.",
+      category: "cars",
+      location: "Oulu",
+      images: "no",
+      price: "123",
+      date: "27.02.2021 15:42",
+      delivery: "pickup",
+      seller: "Jack",
+      contact: "abc@zfsd.com / 050 123 123 123"
+    },
+    {
+      id: 3,
+      title: "Old Toyota Corolla3",
+      description: "Long description here",
+      category: "cars",
+      location: "Oulu",
+      images: "no",
+      price: "123",
+      date: "27.02.2021 15:42",
+      delivery: "pickup",
+      seller: "Jack",
+      contact: "abc@zfsd.com / 050 123 123 123"
+    },
+    {
+      id: 4,
+      title: "Old Toyota Corolla4",
+      description: "Selling my Toyota Corolla Le, with only 90k original miles and Clean Title, never been any accidents, garage keeping.",
+      category: "cars",
+      location: "Oulu",
+      images: "no",
+      price: "123",
+      date: "27.02.2021 15:42",
+      delivery: "pickup",
+      seller: "Jack",
+      contact: "abc@zfsd.com / 050 123 123 123"
+    },
+    {
+      id: 5,
+      title: "Old Toyota Corolla5",
+      description: "Selling my Toyota Corolla Le, with only 90k original miles and Clean Title, never been any accidents, garage keeping.",
+      category: "cars",
+      location: "Oulu",
+      images: "no",
+      price: "123",
+      date: "27.02.2021 15:42",
+      delivery: "pickup",
+      seller: "Jack",
+      contact: "abc@zfsd.com / 050 123 123 123"
+    },
+    {
+      id: 6,
+      title: "Old Toyota Corolla6",
+      description: "Selling my Toyota Corolla Le, with only 90k original miles and Clean Title, never been any accidents, garage keeping.",
+      category: "cars",
+      location: "Oulu",
+      images: "no",
+      price: "123",
+      date: "27.02.2021 15:42",
+      delivery: "pickup",
+      seller: "Jack",
+      contact: "abc@zfsd.com / 050 123 123 123"
+ },
+ {
+    id: 7,
+    title: "Shoes",
+    description: "My old shoes",
+    category: "clothes",
+    location: "Oulu",
+    images: "no",
+    price: "123",
+    date: "27.02.2021 15:42",
+    delivery: "pickup",
+    seller: "Jack",
+    contact: "abc@zfsd.com / 050 123 123 123"
+}
+];
+
+
+
+
 
 
 
@@ -32,17 +152,17 @@ app.use(cors());
 
 
 // for graded ex:
-app.post('/users/login', (req, res) => res.send('user login!'));
+//app.post('/users/login', (req, res) => res.send('user login!'));
 //email
 //password
 
-app.post('/users/register', (req, res) => res.send('user register!'));
+//app.post('/users/register', (req, res) => res.send('user register!'));
 //email
 //firstname
 //lastname
 //password
 
-app.post('/postings/add', (req, res) => res.send('add posting!'));
+//app.post('/postings/add', (req, res) => res.send('add posting!'));
 //title
 //description
 //dategory
@@ -53,10 +173,10 @@ app.post('/postings/add', (req, res) => res.send('add posting!'));
 //delivery_type
 //seller_id
 
-app.post('/postings/delete', (req, res) => res.send('delete posting!'));
+//app.post('/postings/delete', (req, res) => res.send('delete posting!'));
 //posting_id
 
-app.post('/postings/edit', (req, res) => res.send('edit posting!'));
+//app.post('/postings/edit', (req, res) => res.send('edit posting!'));
 
 //app.post('/postings/search', (req, res) => res.send('edit posting!'));
 
@@ -85,10 +205,10 @@ app.route('/world')
     .delete((req, res) => res.send('delete World'))
 
 /* demonstrate route module/component usage - the dogComponent content is defined in separate file */
-app.use('/dogs', dogComponent);
-app.use('/apiKey', apiKeyDemo);
-app.use('/fileUpload', imageUpload);
-app.use('/jsonSchemaValidationExample', jsonSchemaValidationExample);
+app.use('/postings', postingsComponent);
+//app.use('/apiKey', apiKeyDemo);
+//app.use('/fileUpload', imageUpload);
+//app.use('/jsonSchemaValidationExample', jsonSchemaValidationExample);
 
 /* This will be activated as the last if no other route matches. */
 app.use((req, res, next) => {
